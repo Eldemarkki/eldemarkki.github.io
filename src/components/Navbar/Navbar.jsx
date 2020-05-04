2c6d238717ae090c64679a9b216c51f86732bbe0
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2020-05-04 21:51:29 +0300

    Made navbar not sticky

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,32 +10,13 @@ class Navbar extends React.Component {
     }
   }
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll.bind(this));
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
-
   render() {
     return (
-      <div id="navbar" className={this.state.sticked ? "sticky" : ""}>
+      <div id="navbar">
         {this.props.children}
       </div>
     );
   }
-
-  handleScroll() {
-    // TODO: Change window.innerHeight to the navbar's actual top
-    if (window.scrollY * 2 < window.innerHeight && this.state.sticked === true) {
-      this.setState({ sticked: false });
-    }
-    else if (window.scrollY * 2 > window.innerHeight && this.state.sticked !== true) {
-      this.setState({ sticked: true });
-    }
-  }
-
 }
 
 export default Navbar;
